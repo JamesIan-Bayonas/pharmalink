6536b0e8ab3870231040102782f4d0ad5d891b1c
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-11-25 15:44:17 +0800

    feat(middleware): add ExceptionMiddleware to handle global exceptions

---
+++ PharmaLink.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace PharmaLink.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error from the custom middleware.",
+                Detailed = exception.Message
+            };
+
+            return context.Response.WriteAsync(System.Text.Json.JsonSerializer.Serialize(response));
+        }
+    }
+}
