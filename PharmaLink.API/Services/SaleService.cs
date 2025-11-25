a6124dbc8fa83e732c635bcbafeae6c2e7273f48
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-11-25 16:29:44 +0800

    feat(services): add SaleService to validate user existence in database

---
+++ PharmaLink.API/Services/SaleService.cs
@@ -0,0 +1,58 @@
+﻿using PharmaLink.API.DTOs.Sales;
+using PharmaLink.API.Entities;
+using PharmaLink.API.Interfaces;
+//using PharmaLink.API.Repositories;
+
+namespace PharmaLink.API.Services
+{
+    public class SaleService : ISaleService
+    {
+        private readonly ISaleRepository _saleRepository;
+        private readonly IMedicineRepository _medicineRepository; // Needed to check prices/stock
+
+        public SaleService(ISaleRepository saleRepo, IMedicineRepository medicineRepo)
+        {
+            _saleRepository = saleRepo;
+            _medicineRepository = medicineRepo;
+        }
+
+        public async Task<int> ProcessSaleAsync(int userId, CreateSaleRequestDto request)
+        {
+            decimal totalAmount = 0;
+            var saleItemsEntities = new List<SaleItem>();
+
+            foreach (var itemDto in request.Items)
+            {
+                // 1. Validate Medicine Exists and Check Stock
+                var medicine = await _medicineRepository.GetByIdAsync(itemDto.MedicineId);
+                if (medicine == null)
+                    throw new Exception($"Medicine ID {itemDto.MedicineId} not found.");
+
+                if (medicine.StackQuantity < itemDto.Quantity)
+                    throw new Exception($"Insufficient stock for {medicine.Name}. Available: {medicine.StackQuantity}");
+
+                // 2. Calculate Costs
+                totalAmount += medicine.Price * itemDto.Quantity;
+
+                // 3. Prepare Entity
+                saleItemsEntities.Add(new SaleItem
+                {
+                    MedicinesId = itemDto.MedicineId,
+                    Quantity = itemDto.Quantity,
+                    UnitPrice = medicine.Price
+                });
+            }
+
+            // 4. Create Sale Header Entity
+            var saleEntity = new Sale
+            {
+                UserId = userId,
+                TotalAmount = totalAmount,
+                TransDate = DateTime.Now
+            };
+
+            // 5. Execute Transaction
+            return await _saleRepository.CreateSaleTransactionAsync(saleEntity, saleItemsEntities);
+        }
+    }
+}
