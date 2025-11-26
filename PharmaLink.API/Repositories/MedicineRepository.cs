a1f03b6f1b73be9ce7fc5415931f96380f73c46e
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-11-26 23:04:22 +0800

    git(repositories): add MedicineRepository implementation using Dapper

---
+++ PharmaLink.API/Repositories/MedicineRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using PharmaLink.API.Entities;
+using PharmaLink.API.Interfaces;
+using System.Data;
+
+public class MedicineRepository : IMedicineRepository
+{
+    private readonly string _connectionString;
+
+    public MedicineRepository(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public async Task<Medicine?> GetByIdAsync(int id)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            string sql = "SELECT * FROM Medicines WHERE Id = @Id";
+            return await connection.QuerySingleOrDefaultAsync<Medicine>(sql, new { Id = id });
+        }
+    }
+
+    public async Task<IEnumerable<Medicine>> GetAllAsync()
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            string sql = "SELECT * FROM Medicines";
+            return await connection.QueryAsync<Medicine>(sql);
+        }
+    }
+
+    public async Task<int> CreateAsync(Medicine medicine)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            string sql = @"
+                INSERT INTO Medicines (CategoryId, Name, StackQuantity, Price)
+                VALUES (@CategoryId, @Name, @StackQuantity, @Price);
+                SELECT CAST(SCOPE_IDENTITY() as int);";
+            return await connection.QuerySingleAsync<int>(sql, medicine);
+        }
+    }
+
+    public async Task<bool> UpdateStockAsync(int id, int quantityDeducted, IDbTransaction transaction = null)
+    {
+        string sql = "UPDATE Medicines SET StackQuantity = StackQuantity - @Quantity WHERE Id = @Id";
+        var parameters = new { Quantity = quantityDeducted, Id = id };
+
+        // IF a transaction is passed (like from SaleService), use it.
+        if (transaction != null)
+        {
+            int rows = await transaction.Connection.ExecuteAsync(sql, parameters, transaction);
+            return rows > 0;
+        }
+        else
+        {
+            // ELSE open a new independent connection.
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                int rows = await connection.ExecuteAsync(sql, parameters);
+                return rows > 0;
+            }
+        }
+    }
+}
