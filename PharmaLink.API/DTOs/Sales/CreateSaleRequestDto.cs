c59a91595fa548c6b33b9a20971560b0562f000c
Faith12309 <alyssa_bagunbon@dmc.edu.ph>
2025-11-27 14:13:11 +0800

    feat(auth): add CreateSaleRequestDto, SaleItemDTo, to create user request and display sale items

---
+++ PharmaLink.API/DTOs/Sales/CreateSaleRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaLink.API.DTOs.Sales
+{
+    public class CreateSaleRequestDto
+    {
+        [Required]
+        [MinLength(1, ErrorMessage = "Cart cannot be empty")]
+        public List<SaleItemDto>? Items { get; set; }
+    }
+}
---
+++ PharmaLink.API/DTOs/Sales/SaleItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaLink.API.DTOs.Sales
+{
+
+    public class SaleItemDto
+    {
+        [Required]
+        public int MedicineId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
+        public int Quantity { get; set; }
+    }
+}
