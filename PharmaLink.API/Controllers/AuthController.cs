c94564259b7376c7048733cd4919703f781d7d8e
Faith12309 <alyssa_bagunbon@dmc.edu.ph>
2025-11-27 14:20:10 +0800

    feat(controllers): add AuthController to add user validation

---
+++ PharmaLink.API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿namespace PharmaLink.API.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using PharmaLink.API.DTOs.Auth;
+    using PharmaLink.API.Entities;
+    using PharmaLink.API.Interfaces;
+
+    // 1. Auth Controller (New!)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDto request)
+        {
+            try
+            {
+                var newUser = new User { UserName = request.UserName };
+                var result = await _authService.RegisterAsync(newUser, request.Password);
+                return Ok(new { message = result });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDto request)
+        {
+            var token = await _authService.LoginAsync(request.Username, request.Password);
+
+            if (token == null)
+                return Unauthorized(new { message = "Invalid username or password" });
+
+            return Ok(new { token = token });
+        }
+    }
+
+
+}
