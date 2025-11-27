305fdea41980e60a0de27433767f8acb1241984b
Faith12309 <alyssa_bagunbon@dmc.edu.ph>
2025-11-27 14:09:44 +0800

    feat(auth): add UserLoginDto, UserRegisterDto to handle user validation

---
+++ PharmaLink.API/DTOs/Auth/UserLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaLink.API.DTOs.Auth
+{
+    public class UserLoginDto
+    {
+        [Required]
+        public required string Username { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+    }
+}
---
+++ PharmaLink.API/DTOs/Auth/UserRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿namespace PharmaLink.API.DTOs.Auth
+{
+    using System.ComponentModel.DataAnnotations;
+
+    // 1. Auth DTOs
+    public class UserRegisterDto
+    {
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string? Password { get; set; }
+    }
+}
