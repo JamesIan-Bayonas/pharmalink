4cff01ed4183b3014cd4c0fff0512e3931211a5d
nb5002 <bendijonena176@gmail.com>
2025-11-27 16:14:20 +0800

    feat(entities): degfine Medicine for user authentication

--- PharmaLink.API/Entities/Medicine.cs
+++ PharmaLink.API/Entities/Medicine.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int CategoryId { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; } = null!;
         public int StackQuantity { get; set; }
         public decimal Price { get; set; }
     }
