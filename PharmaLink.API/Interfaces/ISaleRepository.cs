56ab543475d82ceea2b3a3be840e9091495115f8
Faith12309 <alyssa_bagunbon@dmc.edu.ph>
2025-12-01 23:54:10 +0800

    feat(interfaces): add GetAllAsync interface for fetching requested user output

--- PharmaLink.API/Interfaces/ISaleRepository.cs
+++ PharmaLink.API/Interfaces/ISaleRepository.cs
@@ -7,5 +7,6 @@ namespace PharmaLink.API.Interfaces
         // The main transaction method used in the core flow
         Task<int> CreateSaleTransactionAsync(Sale sale, List<SaleItem> items);
         Task<Sale?> GetByIdAsync(int id);
+        Task<IEnumerable<Sale>> GetAllAsync();
     }
 }
