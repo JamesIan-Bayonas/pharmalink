fb301b3a6068bce19dde4eda345fc051c151a271
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-12-02 15:57:36 +0800

    feat(services): implement CategoryService with DTO functionality integration and CRUD operations >> - Add CreateCategoryAsync, GetAllCategoriesAsync, and DeleteCategoryAsync. >> - centralize DTO (Create and Responce) to Entity mapping logic >> - integrate ICategoryRepository for data persistence

---
+++ PharmaLink.API/Services/CategoryService.cs
@@ -0,0 +1,40 @@
+﻿using PharmaLink.API.DTOs.Categories;
+using PharmaLink.API.Entities;
+using PharmaLink.API.Interfaces;
+
+namespace PharmaLink.API.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<int> CreateCategoryAsync(CreateCategoryDto request)
+        {
+            var newCategory = new Category
+            {
+                Name = request.Name
+            };
+            return await _categoryRepository.CreateAsync(newCategory);
+        }
+
+        public async Task<IEnumerable<CategoryResponseDto>> GetAllCategoriesAsync()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            return categories.Select(c => new CategoryResponseDto
+            {
+                Id = c.Id,
+                Name = c.Name
+            });
+        }
+
+        public async Task<bool> DeleteCategoryAsync(int id)
+        {
+            return await _categoryRepository.DeleteAsync(id);
+        }
+    }
+}
