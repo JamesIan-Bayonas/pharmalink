8e0d199547999173cc52301b116235e702610c7a
JayAn1302 <jay-an_calago@dmc.edu.ph>
2025-12-02 23:01:38 +0800

    feat(entities): add properties ExpiryDate

--- PharmaLink.API/Entities/Medicine.cs
+++ PharmaLink.API/Entities/Medicine.cs
@@ -3,9 +3,10 @@
     public class Medicine
     {
         public int Id { get; set; }
-        public int CategoryId { get; set; }
         public string Name { get; set; } = null!;
-        public int StackQuantity { get; set; }
+        public int CategoryId { get; set; }
+        public int StockQuantity { get; set; }
         public decimal Price { get; set; }
+        public DateTime ExpiryDate { get; set; }
     }
 }
