31fce217e964bab00b50459db804a5c042532fc2
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-12-05 22:35:06 +0800

    feat(dtos): add UserResponce for contaning user input

---
+++ PharmaLink.API/DTOs/Users/UserResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PharmaLink.API.DTOs.Users
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Role { get; set; }
+    }
+}
