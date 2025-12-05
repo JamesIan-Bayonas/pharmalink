2b98665bc658fdc5149bda709e3ee0841d7b4c21
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-12-05 20:42:45 +0800

    feat(interfaces): add ICategoryRepository, ICategoryService, IMedicineService, ITokenService

---
+++ PharmaLink.API/Interfaces/IMedicineService.cs
@@ -0,0 +1,11 @@
+﻿using PharmaLink.API.DTOs.Medicines;
+
+namespace PharmaLink.API.Interfaces
+{
+    public interface IMedicineService
+    {
+        Task<IEnumerable<MedicineResponseDto>> GetAllMedicinesAsync();
+        Task<MedicineResponseDto?> GetMedicineByIdAsync(int id);
+        Task<int> CreateMedicineAsync(CreateMedicineDto request);
+    }
+}
---
+++ PharmaLink.API/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using PharmaLink.API.Entities;
+using PharmaLink.API.Interfaces;
+
+namespace PharmaLink.API.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateToken(User user);
+    }
+}
