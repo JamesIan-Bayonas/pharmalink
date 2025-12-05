012b1266ec4cb73c66b4a6f6b2e2a8f2d5c15f65
JayAn1302 <jay-an_calago@dmc.edu.ph>
2025-12-05 19:00:27 +0800

    feat(interfaces): add ITokenService with method: GenerateToken

---
+++ PharmaLink.API/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using PharmaLink.API.Entities;
+using PharmaLink.API.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PharmaLink.API.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var jwtKey = _configuration["JwtSettings:Key"] ?? "this_is_a_very_long_super_secret_key_for_pharmalink_security";
+            var key = Encoding.UTF8.GetBytes(jwtKey);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim("uid", user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role) // Add Role claim!
+            };
+
+            var creds = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7), // Make it last longer
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
+
