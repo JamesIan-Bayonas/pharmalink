21010c9a9f4dcd9441b51571363f71810f602541
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-12-05 22:37:07 +0800

    feat(dtos): add CreateMedicineDto, MedicineResponseDto for DTO for safety additional security layer

---
+++ PharmaLink.API/DTOs/Medicines/MedicineResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace PharmaLink.API.DTOs.Medicines
+{
+    public class MedicineResponseDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int CategoryId { get; set; }
+        public int StockQuantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
