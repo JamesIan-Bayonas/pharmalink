df778dc5674baa7c984bda29cae6ca3759f2cfd8
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-12-06 16:04:40 +0800

    feat(sales): add SaleResponseDto

---
+++ PharmaLink.API/DTOs/Sales/SaleResponseDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PharmaLink.API.DTOs.Sales
+{
+    public class SaleResponseDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public List<SaleItemResponseDto> Items { get; set; } = new List<SaleItemResponseDto>();
+    }
+
+    public class SaleItemResponseDto
+    {
+        public int Id { get; set; }
+        public int MedicineId { get; set; }
+        public string MedicineName { get; set; } // fetch this via JOIN
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal SubTotal => Quantity * UnitPrice;
+    }
+}
