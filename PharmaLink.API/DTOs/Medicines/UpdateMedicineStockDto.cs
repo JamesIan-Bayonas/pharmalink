64bcd7c960c211ea6ba3ce2bbefa8969b482d588
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-12-07 22:21:02 +0800

    feat(dtos): add UpdateMedicineStockDto calculating logic

---
+++ PharmaLink.API/DTOs/Medicines/UpdateMedicineStockDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaLink.API.DTOs.Medicines
+{
+    public class UpdateMedicineStockDto
+    {
+        [Required]
+        public required int Quantity { set; get; }
+    }
+}
