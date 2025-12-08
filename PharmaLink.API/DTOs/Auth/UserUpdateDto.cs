2c65a22b7308ab08a626424e15c43217728282ff
Faith12309 <alyssa_bagunbon@dmc.edu.ph>
2025-12-08 14:19:43 +0800

    feat(auth): add UserUpdateDto to handle update logic towards User credintials

---
+++ PharmaLink.API/DTOs/Auth/UserUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PharmaLink.API.DTOs.Auth
+{
+    public class UserUpdateDto
+    {
+        [Required]
+        public string Username { get; set; } = null!;
+
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string? Password { get; set; } // Optional: Leave null if not changing
+    }
+}
