51cdf94afd29a416ff69754f93e11851b1e87412
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-12-17 22:13:39 +0800

    feat(utilities): add Interface for autoMapper injection

---
+++ PharmaLink.API/Utilities/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using PharmaLink.API.Entities;
+using PharmaLink.API.DTOs.Auth;
+using PharmaLink.API.DTOs.Users;
+using PharmaLink.API.DTOs.Categories;
+using PharmaLink.API.DTOs.Medicines;
+
+namespace PharmaLink.API.Utilities
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // --- AUTH & USERS ---
+            CreateMap<UserRegisterDto, User>(); // Register (DTO -> Entity)
+            CreateMap<User, UserResponseDto>(); // Get Users (Entity -> DTO)
+
+            // --- CATEGORIES ---
+            CreateMap<CreateCategoryDto, Category>(); // Create
+            CreateMap<Category, CategoryResponseDto>(); // Read
+
+            // --- MEDICINES ---
+            CreateMap<CreateMedicineDto, Medicine>(); // Create
+            CreateMap<UpdateMedicineDto, Medicine>(); // Update
+            CreateMap<Medicine, MedicineResponseDto>(); // Read
+        }
+    }
+}
