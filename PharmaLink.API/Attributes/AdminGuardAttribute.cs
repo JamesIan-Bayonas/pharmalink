9f17f1a5df4d5da2027108aae0bcb293876cc9bd
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-12-17 22:14:36 +0800

    feat(attributes): add AdminGuardAttributes for validation messages

---
+++ PharmaLink.API/Attributes/AdminGuardAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace PharmaLink.API.Attributes
+{
+    // This attribute can be applied to Methods or Classes
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class AdminGuardAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly string _customMessage;
+        public AdminGuardAttribute(string message)
+        {
+            _customMessage = message;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+
+            // 1. Check if they are logged in at all
+            if (!user.Identity.IsAuthenticated)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            // 2. Check if they are an Admin
+            if (!user.IsInRole("Admin"))
+            {
+                // 3. If not, REJECT them immediately with your custom 403 message
+                context.Result = new ObjectResult(new
+                {
+                    message = _customMessage,
+                    error = "Forbidden Access"
+                })
+                {
+                    StatusCode = 403
+                };
+            }
+        }
+    }
+}
