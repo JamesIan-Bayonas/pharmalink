e302020d458855392aacbd2b9e2da5986b3c945b
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-12-26 14:13:16 +0800

    feat(sales): implement SalesParams properties for enhancing pagination features

---
+++ PharmaLink.API/DTOs/Sales/SalesParams.cs
@@ -0,0 +1,12 @@
+﻿namespace PharmaLink.API.DTOs.Sales
+{
+    public class SalesParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+        public int PageSize { get => _pageSize; set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
